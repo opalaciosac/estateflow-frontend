f6d44187f36ba5063120da9034025adef8983226
opalaciosac <opalacios@affinityconsulting.com>
2025-09-27 23:05:11 -0600

    Second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Ajusta la URL según donde corra tu Azure Function local
+// URL = Azure Function in local
 export default defineConfig({
   plugins: [react()],
   server: {
